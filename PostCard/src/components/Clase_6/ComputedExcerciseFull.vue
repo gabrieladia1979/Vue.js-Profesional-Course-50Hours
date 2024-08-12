916754ff1f7c02d680c1e1cd84a2b513c67dc6f8
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-12 18:04:54 -0300

    Create ComputedExcerciseFull.vue
    Trying to do a better perfornmance

---
+++ PostCard/src/components/Clase_6/ComputedExcerciseFull.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label for="subtTotal">Subtotal</label>
+    <input type = 'number' v-model = 'subtTotal'>
+    <p>Total de impuestos aplicados {{TotalVat}} {{vat}}%</p>
+    <botton v-on:@click = "AddProduct">Agregar Producto</botton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    import {ref , Ref ,  computed} from 'vue'
+
+    let subtTotal:Ref<number> = ref(0)
+    const vat = 21
+    const TotalVat = computed(() => (vat*subtTotal.value)/100)
+
+    let AddProduct = () => {
+      subtTotal.value += 1
+    }
+
+    interface IProduct {
+      price:number,
+      quantity:number
+    }
+
+    let products:Ref<IProduct> =  ref([{
+        price:0, quantity:0}]);
+
+</script>
+
+<style scoped>
+
+</style>
