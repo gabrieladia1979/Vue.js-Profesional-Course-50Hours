de7c4f99716fa7a1d965ec4168795a4d0c81c386
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-14 15:59:10 -0300

    Update main.ts Directive
    Font SIze

--- PostCard/src/main.ts
+++ PostCard/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.directive('font-size', 
+    {beforeMount: (el,binding) => {
+        el.style.fontSize = '70px'
+    
+    }}
+)
+
+app.mount('#app')
