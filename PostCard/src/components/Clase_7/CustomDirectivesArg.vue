fb3999d1d23ff7d79fe5e07c59cb6dc5d932a4c4
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-15 20:17:38 -0300

    Main.ts Directives Arg
    Main.ts Directives Arg and New Directives with Arg and Binding values . Use of SWITCH

---
+++ PostCard/src/components/Clase_7/CustomDirectivesArg.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <label for="subtTotal" v-custom-size:sm = '100'>Subtotal</label>
+      <input type="number" v-model="subtTotal">
+      <p v-custom-size:xxl>Total impuestos a pagar: {{ TotalVat }}  ({{ vat }})%</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref, Ref, computed } from 'vue';
+
+    const vat = 21; // valor constante
+    let subtTotal: Ref<number> = ref(0);
+    const TotalVat = computed(() => (vat * subtTotal.value) / 100);
+</script>
+
+<style scoped>
+</style>
--- PostCard/src/main.ts
+++ PostCard/src/main.ts
@@ -10,9 +10,44 @@ app.directive('font-size',
     }}
 )
 
+//app.directive('custom-size',{    --caso 1 solo binding
+//    beforeMount: (el,binding) =>{
+//        el.style.fontSize = binding.value + 'px'
+//    }
+//})
+
+
+// caso 2 con argumento
 app.directive('custom-size',{
-    beforeMount: (el,binding) =>{
-        el.style.fontSize = binding.value + 'px'
+
+    beforeMount: (el,binding) => {
+    
+        let size = 18
+    
+        switch(binding.arg){
+            
+            case 'sm':
+                size = 10
+                break
+            
+            case 'md':
+                size = 18
+                break   
+            
+            case 'lg':
+                size = 24
+                break
+           
+            case 'xl':
+                size = 36
+                break
+                
+            case 'xxl':
+                size = 48
+            
+            
+        }
+    el.style.fontSize = size + 'px'
     }
 })
 
