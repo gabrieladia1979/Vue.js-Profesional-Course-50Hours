a02c5ad5974cb93c7b987d404e9386a9bfeb347d
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-20 16:59:53 -0300

    Fix bug in Services Proyect
    Fixes a bug that caused  problem in proyect 3 to proyect 8

--- servicios_proyecto_clase_8/src/App.vue
+++ servicios_proyecto_clase_8/src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<PostList />
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script lang="ts" setup>
+import PostList from './components/PostList.vue';
 </script>
 
 <style>
---
+++ servicios_proyecto_clase_8/src/components/PostList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hola mundo</div>
+</template>
+
+<script lang="ts" setup>
+    
+</script>
+
+<style scoped>
+
+</style>
