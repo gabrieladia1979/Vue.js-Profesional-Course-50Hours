55930d59ebacfad8bef233fb2579bdcf4af2adb6
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-21 15:13:05 -0300

    Update PostList.vue
    Fix v for

--- serviceClase8/src/components/PostList.vue
+++ serviceClase8/src/components/PostList.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>Hola mundo</div> //falta hacer el v-for
+  <h1>Post List</h1>
+  <ul class = 'post-list'>
+    <li v-for = 'post in post' :key = 'post.id'>
+      {{ post.title }}
+    </li>
+  </ul>
+
 </template>
 
 <script lang="ts" setup>
+
     import PostService from '@/services/PostService';
     import { ref, Ref , onMounted } from 'vue';
 
-    const service = new PostService(); 
-    const post = service.getPosts();
+    const service = new PostService(); // Instancia de la clase PostService.
+    const post = service.getPosts(); // Traigo los post del servicio.
 
     onMounted  ( async() => {
     
-      await service.fetchAllPosts();
+      await service.fetchAllPosts(); // Aca
     
     })
 
