c70d84d135233ec2ff742555c6b40a888f28e06c
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-20 22:33:51 -0300

    Pull Component Service
    Fix service error and try to solve v-for . Need revision.

--- serviceClase8/src/components/PostList.vue
+++ serviceClase8/src/components/PostList.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>Hola mundo</div>
+  <div>Hola mundo</div> //falta hacer el v-for
 </template>
 
 <script lang="ts" setup>
+    import PostService from '@/services/PostService';
+    import { ref, Ref , onMounted } from 'vue';
+
+    const service = new PostService(); 
+    const post = service.getPosts();
+
+    onMounted  ( async() => {
     
+      await service.fetchAllPosts();
+    
+    })
+
 </script>
 
 <style scoped>
