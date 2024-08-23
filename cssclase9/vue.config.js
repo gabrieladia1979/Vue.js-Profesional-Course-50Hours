94d314e7b43b2f5ddb3bd05b50aa9eb025c52dbc
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-23 10:50:17 -0300

    New Snippet and Global SCSS

--- cssclase9/vue.config.js
+++ cssclase9/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true, // solo indicar para transpilar dependencias
+  css: {
+    
+    loaderOptions:{ // Moficaciones de carga de css
+      
+      sass: {
+      
+        additionalData: `@import "@/scss/_variables.scss";` //La comilla invertida nos permite escribir codigo
+      
+      }
+
+     }
+  }
 })
