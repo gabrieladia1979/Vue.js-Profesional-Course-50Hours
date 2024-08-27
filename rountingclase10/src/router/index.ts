8e534fcf1056fbff8436e02bb6743e2bcb724b1a
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-27 14:13:30 -0300

    Modificacion de listado de rutas

--- rountingclase10/src/router/index.ts
+++ rountingclase10/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   }
 ]
 
