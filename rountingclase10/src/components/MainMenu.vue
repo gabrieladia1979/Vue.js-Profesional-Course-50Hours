4b73cc9fe1d639f887be1dcf423219ea4c90a42a
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-27 21:01:22 -0300

    New component Navbar
    MainMenu

---
+++ rountingclase10/src/components/MainMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/services">Services</router-link> |
+      <router-link to="/contact">Contact</router-link> |
+    </nav>
+  </template>
+
+<script lang="ts" setup>
+      
+</script>
+
+<style scoped lang="scss">
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
