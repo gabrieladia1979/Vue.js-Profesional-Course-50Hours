4653a0f802496ff2be6205e4dc5379bba3782bba
gabrieladia1979 <gadia@uade.edu.ar>
2024-08-30 10:36:35 -0300

    New way to use route link
    better way to make a stong code

--- rountingclase10/src/views/BlogView.vue
+++ rountingclase10/src/views/BlogView.vue
@@ -3,14 +3,17 @@
     <h1>Blog Section</h1>
     <ul>
       <li>
-        <router-link to="/blog/post/1">Primer Post</router-link>
+        <router-link :to="{name: 'post' , params: {id:1}}">Primer Post</router-link>
       </li>
       <li>
-        <router-link to="/blog/post/2">Post 2</router-link>
+        <router-link :to="{name: 'post' , params: {id:2}}"</router-link>
       </li>
       <li>
-        <router-link to="/blog/post/3">Post 3</router-link>
+        <router-link :to="{name: 'post' , params: {id:3}}">Post 3</router-link>
       </li> 
+      <li>
+        <router-link :to="{name: 'post' , params: {id:4}}">Post 4</router-link>
+      </li>
     </ul>
   </div>
 </template>
